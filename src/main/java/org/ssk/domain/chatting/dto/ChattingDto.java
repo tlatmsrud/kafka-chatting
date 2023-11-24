6567adc552eea9cbba5508f02ec8cd77bdab4e41
tlatmsrud <tlatmsrud@naver.com>
2023-11-24 17:33:56 +0900

    :recycle: Use-static-factory-method-pattern

--- src/main/java/org/ssk/domain/chatting/dto/ChattingDto.java
+++ src/main/java/org/ssk/domain/chatting/dto/ChattingDto.java
@@ -2,8 +2,6 @@ package org.ssk.domain.chatting.dto;
 
 import lombok.Getter;
 
-import java.time.LocalDateTime;
-
 /**
  * title        :
  * author       : sim
@@ -15,5 +13,15 @@ import java.time.LocalDateTime;
 public class ChattingDto {
     private String sessionId;
     private String message;
-    private LocalDateTime localDateTime;
+    private String time;
+
+    private ChattingDto(String sessionId, String message, String time){
+        this.sessionId = sessionId;
+        this.message = message;
+        this.time = time;
+    }
+
+    public static ChattingDto of(String sessionId, String message, String time){
+        return new ChattingDto(sessionId, message, time);
+    }
 }
