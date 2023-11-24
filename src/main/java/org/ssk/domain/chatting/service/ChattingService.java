14291b463fb628b25394ad3e15078e6fd0a69d39
tlatmsrud <tlatmsrud@naver.com>
2023-11-24 17:37:07 +0900

    :sparkles: create-chatting-room-function
    채팅 룸 관련 서비스 기능 구현

--- src/main/java/org/ssk/domain/chatting/service/ChattingService.java
+++ src/main/java/org/ssk/domain/chatting/service/ChattingService.java
@@ -1,9 +1,18 @@
 package org.ssk.domain.chatting.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.ssk.domain.chatting.domain.ChattingRoom;
+import org.ssk.domain.chatting.dto.ChattingDto;
+import org.ssk.domain.chatting.dto.ChattingRoomDto;
 import org.ssk.domain.chatting.dto.SendDto;
 import org.ssk.domain.chatting.record.ChattingRecord;
+import org.ssk.domain.chatting.repository.ChattingRoomRepository;
+import org.ssk.domain.chatting.strategy.ChattingSelectStrategy;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * title        :
@@ -14,14 +23,39 @@ import org.ssk.domain.chatting.record.ChattingRecord;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class ChattingService {
 
     private final KafkaProducerService kafkaProducerService;
+    private final ChattingRoomRepository chattingRoomRepository;
+    private final ChattingSelectStrategy chattingSelectStrategy;
 
-    public void send(SendDto sendDto){
+    public List<ChattingRoomDto> getChattingRoomList(){
+        return chattingRoomRepository.findAll().stream()
+                .map(entity -> ChattingRoomDto.of(entity.getRoomId(), entity.getRoomName()))
+                .collect(Collectors.toList());
+    }
+    public void createChattingRoom(String chattingRoomName){
+        ChattingRoom chattingRoom = ChattingRoom.builder().roomName(chattingRoomName).build();
+        chattingRoomRepository.save(chattingRoom);
+        log.info("created chatting room : "+ chattingRoomName);
+    }
+    public void send(SendDto sendDto, String sessionId){
+        Long roomId = sendDto.getRoomId();
 
-        String roomId = sendDto.getRoomId();
+        if(!isChattingRoom(roomId)){
+            throw new RuntimeException("room is not exists");
+        }
+
         kafkaProducerService.sendChattingRecord(roomId
-                , ChattingRecord.of(sendDto.getSessionId(), sendDto.getMessage()));
+                , ChattingRecord.of(sessionId, sendDto.getMessage()));
+    }
+
+    public boolean isChattingRoom(Long roomId){
+        return chattingRoomRepository.existsById(roomId);
+    }
+
+    public List<ChattingDto> getChattingListByRoomId(Long roomId){
+        return chattingSelectStrategy.selectChattingByRoomId(roomId);
     }
 }
