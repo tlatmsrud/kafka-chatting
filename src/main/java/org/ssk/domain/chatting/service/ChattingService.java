aa83d5e1b8ffe6d6d824e9cd0630db32c7f7faab
tlatmsrud <tlatmsrud@naver.com>
2023-11-24 15:15:24 +0900

    :truck: Rename-kafkaService-to-kafkaProducerService

--- src/main/java/org/ssk/domain/chatting/service/ChattingService.java
+++ src/main/java/org/ssk/domain/chatting/service/ChattingService.java
@@ -1,10 +1,7 @@
 package org.ssk.domain.chatting.service;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Service;
-import org.springframework.util.concurrent.ListenableFuture;
 import org.ssk.domain.chatting.dto.SendDto;
 import org.ssk.domain.chatting.record.ChattingRecord;
 
@@ -19,12 +16,12 @@ import org.ssk.domain.chatting.record.ChattingRecord;
 @RequiredArgsConstructor
 public class ChattingService {
 
-    private final KafkaService kafkaService;
+    private final KafkaProducerService kafkaProducerService;
 
     public void send(SendDto sendDto){
 
         String roomId = sendDto.getRoomId();
-        kafkaService.sendChattingRecord(roomId
+        kafkaProducerService.sendChattingRecord(roomId
                 , ChattingRecord.of(sendDto.getSessionId(), sendDto.getMessage()));
     }
 }
