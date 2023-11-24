db83499a179f577164c92b8d4f90dac6e9f28b05
tlatmsrud <tlatmsrud@naver.com>
2023-11-24 15:13:38 +0900

    :recycle: Separate-consumer-function
    컨슈머 기능을 분리하였고, 클래스 명을 KafkaProducerService로 변경하였다.

---
+++ src/main/java/org/ssk/domain/chatting/service/KafkaProducerService.java
@@ -0,0 +1,43 @@
+package org.ssk.domain.chatting.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.core.KafkaProducerException;
+import org.springframework.kafka.core.KafkaSendCallback;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.stereotype.Service;
+import org.springframework.util.concurrent.ListenableFuture;
+import org.ssk.domain.chatting.record.ChattingRecord;
+
+/**
+ * title        :
+ * author       : sim
+ * date         : 2023-11-23
+ * description  :
+ */
+
+@Service
+@RequiredArgsConstructor
+public class KafkaProducerService {
+
+    private final static String TOPIC = "chatting";
+    private final KafkaTemplate<String, ChattingRecord> customKafkaTemplate;
+
+    public void sendChattingRecord(String topic, ChattingRecord chattingRecord){
+        ListenableFuture<SendResult<String, ChattingRecord>> future = customKafkaTemplate.send(TOPIC, chattingRecord);
+
+        future.addCallback(new KafkaSendCallback<>() {
+
+            @Override
+            public void onSuccess(SendResult<String, ChattingRecord> result) {
+                System.out.println(result);
+            }
+
+            @Override
+            public void onFailure(KafkaProducerException ex) {
+                ex.printStackTrace();
+            }
+        });
+    }
+
+}
