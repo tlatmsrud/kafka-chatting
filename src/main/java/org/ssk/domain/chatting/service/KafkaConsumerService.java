d8d6ae1ac1e682ff7c9ff83cb69ad64f3b0da511
tlatmsrud <tlatmsrud@naver.com>
2023-11-24 18:11:34 +0900

    :recycle: Separate-chatting-save-function

--- src/main/java/org/ssk/domain/chatting/service/KafkaConsumerService.java
+++ src/main/java/org/ssk/domain/chatting/service/KafkaConsumerService.java
@@ -3,9 +3,7 @@ package org.ssk.domain.chatting.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
-import org.ssk.domain.chatting.domain.Chatting;
 import org.ssk.domain.chatting.record.ChattingRecord;
-import org.ssk.domain.chatting.repository.ChattingRepository;
 
 /**
  * title        :
@@ -19,21 +17,12 @@ import org.ssk.domain.chatting.repository.ChattingRepository;
 public class KafkaConsumerService {
 
     private final String TOPIC = "chatting";
-    private final ChattingRepository chattingRepository;
 
     @KafkaListener(
             topics = TOPIC, groupId = "test-01", containerFactory = "customContainerFactory")
     public void customListener(ChattingRecord chattingRecord){
         System.out.println("recordListener");
         System.out.println(chattingRecord.getSessionId());
-
-        Chatting chatting = Chatting.builder()
-                .sessionId(chattingRecord.getSessionId())
-                .message(chattingRecord.getMessage())
-                .time(chattingRecord.getTime())
-                .build();
-
-        chattingRepository.save(chatting);
     }
 
 
