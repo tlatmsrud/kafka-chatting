8cae5563dd63ba8d04579de645bc0db32d2d0534
tlatmsrud <tlatmsrud@naver.com>
2023-11-27 07:17:56 +0900

    :sparkles: Send-message-to-subscriber-for-websocket
    /subscribe/{roomId}를 구독하고 있는 웹소켓 구독자들에게 메시지 발송

--- src/main/java/org/ssk/domain/chatting/service/KafkaConsumerService.java
+++ src/main/java/org/ssk/domain/chatting/service/KafkaConsumerService.java
@@ -2,6 +2,7 @@ package org.ssk.domain.chatting.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
 import org.ssk.domain.chatting.record.ChattingRecord;
 
@@ -18,11 +19,18 @@ public class KafkaConsumerService {
 
     private final String TOPIC = "chatting";
 
+    private final SimpMessagingTemplate messagingTemplate;
+
     @KafkaListener(
-            topics = TOPIC, groupId = "test-01", containerFactory = "customContainerFactory")
+            topics = TOPIC, groupId = "chatting", containerFactory = "customContainerFactory")
     public void customListener(ChattingRecord chattingRecord){
         System.out.println("recordListener");
         System.out.println(chattingRecord.getSessionId());
+        System.out.println(chattingRecord.getRoomId());
+
+        // 카프카가 TOPIC 데이터를 poll 받아온 후 messagingTemplate을 통해 /subscribe/{roomId}를 구독한 클라이언트로 메시지를 발송한다.
+        messagingTemplate.convertAndSend("/subscribe/"+chattingRecord.getRoomId(),
+                chattingRecord);
     }
 
 
