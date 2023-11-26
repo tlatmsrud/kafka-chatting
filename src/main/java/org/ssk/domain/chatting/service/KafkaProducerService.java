285a5e2db29906517271f98fd9b61efa0507a431
tlatmsrud <tlatmsrud@naver.com>
2023-11-27 07:18:43 +0900

    :fire: Remove-parameter-roomId
    채팅방마다 토픽을 생성할 경우 오버헤드가 많아짐에 따라 roomId 필드 제거

--- src/main/java/org/ssk/domain/chatting/service/KafkaProducerService.java
+++ src/main/java/org/ssk/domain/chatting/service/KafkaProducerService.java
@@ -23,8 +23,8 @@ public class KafkaProducerService {
     private final static String TOPIC = "chatting";
     private final KafkaTemplate<String, ChattingRecord> customKafkaTemplate;
 
-    public void sendChattingRecord(Long roomId, ChattingRecord chattingRecord){
-        ListenableFuture<SendResult<String, ChattingRecord>> future = customKafkaTemplate.send(TOPIC+roomId, chattingRecord);
+    public void sendChattingRecord(ChattingRecord chattingRecord){
+        ListenableFuture<SendResult<String, ChattingRecord>> future = customKafkaTemplate.send(TOPIC, chattingRecord);
 
         future.addCallback(new KafkaSendCallback<>() {
 
