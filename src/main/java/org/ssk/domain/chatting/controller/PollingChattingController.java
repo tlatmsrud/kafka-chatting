97d77a079df7967d28e393a7b9a28e0ac0d60970
tlatmsrud <tlatmsrud@naver.com>
2023-11-27 07:20:46 +0900

    :recycle: Move-polling-method
    채팅 입력 및 채팅 조회 API 메서드는 Polling 방식에서만 사용되므로 ChattingController에서 PollingChattingController로 이동시킴

--- src/main/java/org/ssk/domain/chatting/controller/PollingChattingController.java
+++ src/main/java/org/ssk/domain/chatting/controller/PollingChattingController.java
@@ -1,13 +1,12 @@
 package org.ssk.domain.chatting.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.ssk.domain.chatting.dto.ChattingDto;
+import org.ssk.domain.chatting.dto.SendDto;
 import org.ssk.domain.chatting.service.ChattingService;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 /**
@@ -24,6 +23,22 @@ public class PollingChattingController {
 
     private final ChattingService chattingService;
 
+    /**
+     * 채팅 입력
+     * @param request - HttpServletRequest
+     * @param sendDto - 발송할 채팅 Dto
+     */
+    @ResponseBody
+    @PostMapping("/send")
+    public void send(HttpServletRequest request, @RequestBody SendDto sendDto){
+        chattingService.send(sendDto, request.getSession().getId());
+    }
+
+    /**
+     * 채팅 조회
+     * @param roomId - 채팅방 ID
+     * @return 채팅 리스트
+     */
     @GetMapping("/{roomId}")
     public List<ChattingDto> getChatting(@PathVariable Long roomId){
         return chattingService.getChattingListByRoomId(roomId);
