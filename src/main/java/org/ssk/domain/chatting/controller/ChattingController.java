6ff438728896930f79e058571e957056bca41256
tlatmsrud <tlatmsrud@naver.com>
2023-11-26 19:10:26 +0900

    :sparkles: Add-view-api

--- src/main/java/org/ssk/domain/chatting/controller/ChattingController.java
+++ src/main/java/org/ssk/domain/chatting/controller/ChattingController.java
@@ -1,6 +1,8 @@
 package org.ssk.domain.chatting.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.ssk.domain.chatting.dto.ChattingDto;
 import org.ssk.domain.chatting.dto.ChattingRoomDto;
@@ -17,30 +19,48 @@ import java.util.List;
  * description  :
  */
 
-@RestController
+@Controller
 @RequiredArgsConstructor
 @RequestMapping("/api/chat")
 public class ChattingController {
 
     private final ChattingService chattingService;
 
+    @GetMapping
+    public String getMainPage(){
+        return "main";
+    }
+
+    @ResponseBody
     @PostMapping("/room")
     public void createChattingRoom(@RequestParam(name = "roomName") String roomName){
         chattingService.createChattingRoom(roomName);
     }
 
     @GetMapping("/room")
-    public List<ChattingRoomDto> getChattingRoomList(){
-        return chattingService.getChattingRoomList();
+    public String getChattingRoomList(Model model){
+
+        List<ChattingRoomDto> list = chattingService.getChattingRoomList();
+        model.addAttribute("list", list);
+        return "chattingRoomList";
     }
 
+    @ResponseBody
     @PostMapping("/send")
     public void send(HttpServletRequest request, @RequestBody SendDto sendDto){
         chattingService.send(sendDto, request.getSession().getId());
     }
 
+    @GetMapping("/room/enter/{roomId}")
+    public String enterRoom(Model model, @PathVariable("roomId") Long roomId){
+        model.addAttribute("roomId", roomId);
+        return "chatting";
+    }
+
+
+    @ResponseBody
     @GetMapping("/polling/{roomId}")
-    public List<ChattingDto> polling(@PathVariable("roomId") Long roomId){
+    public  List<ChattingDto> polling(@PathVariable("roomId") Long roomId){
         return chattingService.getChattingListByRoomId(roomId);
     }
 }
