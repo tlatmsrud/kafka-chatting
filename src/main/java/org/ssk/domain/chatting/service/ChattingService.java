983243225110aca807f2db360e6b0846aa474f54
seungkeungsim <tlatmsrud@naver.com>
2023-12-04 18:38:38 +0900

    :fire: Remove-save-logic
    채팅을 DB에 저장하는 역할은 컨슈머 어플리케이션에서 진행되기에 제거하였음

--- src/main/java/org/ssk/domain/chatting/service/ChattingService.java
+++ src/main/java/org/ssk/domain/chatting/service/ChattingService.java
@@ -3,13 +3,11 @@ package org.ssk.domain.chatting.service;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import org.ssk.domain.chatting.domain.Chatting;
 import org.ssk.domain.chatting.domain.ChattingRoom;
 import org.ssk.domain.chatting.dto.ChattingDto;
 import org.ssk.domain.chatting.dto.ChattingRoomDto;
 import org.ssk.domain.chatting.dto.SendDto;
 import org.ssk.domain.chatting.record.ChattingRecord;
-import org.ssk.domain.chatting.repository.ChattingRepository;
 import org.ssk.domain.chatting.repository.ChattingRoomRepository;
 import org.ssk.domain.chatting.strategy.ChattingSelectStrategy;
 import org.ssk.global.util.TimeUtil;
@@ -32,7 +30,6 @@ public class ChattingService {
     private final KafkaProducerService kafkaProducerService;
     private final ChattingRoomRepository chattingRoomRepository;
     private final ChattingSelectStrategy chattingSelectStrategy;
-    private final ChattingRepository chattingRepository;
 
     /**
      * 채팅방 리스트 조회
@@ -64,21 +61,12 @@ public class ChattingService {
         String nickname = sendDto.getNickname();
         String time = TimeUtil.getCurrentTime();
 
-        ChattingRoom findChattingRoom = chattingRoomRepository.findById(roomId)
+        chattingRoomRepository.findById(roomId)
                 .orElseThrow(() -> new RuntimeException("room is not exists"));
 
         ChattingRecord chattingRecord = ChattingRecord.of(nickname, message, roomId, time);
 
         kafkaProducerService.sendChattingRecord(chattingRecord);
-
-        Chatting chatting = Chatting.builder()
-                .nickname(nickname)
-                .message(message)
-                .time(time)
-                .chattingRoom(findChattingRoom)
-                .build();
-
-        chattingRepository.save(chatting);
     }
 
 
